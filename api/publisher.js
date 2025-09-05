3125371f7b8069fafd5badeef20b22bd523d836b
Dev Bash <ikorosamuel1@gmail.com>
2025-09-05 13:58:32 +0100

    fix: improve logging by pretty-printing JSON data in publisher output

--- api/publisher.js
+++ api/publisher.js
@@ -69,7 +69,13 @@ async function publishLoop() {
                     timestamp: now,
                 };
                 await client.publish(plantWaterEventName, receiversId, data);
-                console.log(`Published ${plantWaterEventName}: ${data}`);
+                console.log(
+                    `Published ${plantWaterEventName}: ${JSON.stringify(
+                        data,
+                        null,
+                        2
+                    )}`
+                );
             }
         } catch (e) {
             console.error("Failed to publish water", e);
